4b2ce4be3ecda8b62ea26c5fd5bde5b608dfad2c
ilusha80 <ivkashirtsev@gmail.com>
2024-02-21 21:48:00 +0300

    функции

--- call_my_name.js
+++ call_my_name.js
@@ -25,3 +25,34 @@ do {
     console.log(i);
     i++;
 }while (i < 5);
+
+console.log('---------------------');
+console.log('-------Массивы-------');
+console.log('---------------------');
+
+var num_mas = [1, 2, 3, 4, 5];
+console.log(num_mas[0]);
+console.log(num_mas[3]);
+console.log(num_mas[-1]); //TODO как взять последний элемент массива?
+num_mas.push(6);
+console.log(num_mas[5]);
+
+console.log('---------------------');
+console.log('-------Функции-------');
+console.log('---------------------');
+
+function greet(name) {
+    console.log("Hello, " + name + "!"); // TODO Узнать как форматировать строки
+}
+
+greet("Боби Билибс");
+console.log('---------------------');
+console.log('-------Возврат-------');
+console.log('---------------------');
+
+function add(a, b) {
+    return a + b;
+}
+
+var result = add(3, 5);
+console.log(result)
