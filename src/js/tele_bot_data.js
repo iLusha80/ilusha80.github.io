092806a1b05bace33a3f344e2846d5442233d2bb
ilusha80 <ivkashirtsev@gmail.com>
2024-02-22 02:02:03 +0300

    Обработчик на входа с телеграмм бота

---
+++ src/js/tele_bot_data.js
@@ -0,0 +1,30 @@
+// function getTelegramData() {
+//     let tg = window.Telegram.WebApp;
+//     let data = tg.initDataUnsafe;
+//     console.log(data);
+//     console.log(data.chat.id);
+//     console.log(data.message.text);
+//     console.log('--------------------------------')
+// }
+//
+// document.addEventListener("DOMContentLoaded", getTelegramData);
+// let tg = window.Telegram.WebApp;
+
+function onDOMReady(callback) {
+    document.addEventListener("DOMContentLoaded", callback);
+}
+
+function onPageLoad() {
+    // Получаем элемент textarea
+    const outputTextArea = document.getElementById("outputText");
+    let tg = window.Telegram.WebApp;
+    let data = tg.initDataUnsafe;
+    // Текст, который мы хотим отобразить в textarea
+    const initialText = data.toString();
+
+    // Устанавливаем текст в textarea
+    outputTextArea.value = initialText;
+}
+
+// Вызываем функцию при загрузке DOM
+onDOMReady(onPageLoad);
